fbeeb6af764d976ee9e833947e7fbe47af717fb2
saifulriza123 <mail.saifulriza@gmail.com>
2020-11-07 09:45:43 +0700

    add cors untuk semua

--- index.js
+++ index.js
@@ -3,23 +3,7 @@ const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 const PORT = process.env.PORT || 3000;
-// app.use(cors());
-var whitelist = [
-  "http://localhost",
-  "http://localhost:8080",
-  "https://listening.netlify.app",
-];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(
   "/identity/token",
   createProxyMiddleware({
